2a0df57c0923e6a3805e498eccdc2b82fe9ec29c
Wardes Thiago de Carvalho Costa <wardes.costa@gmail.com>
2021-09-08 21:59:55 -0400

    Updating sever.js

--- server.js
+++ server.js
@@ -1,17 +1,11 @@
-const express = require("express");
-const serveStatic = require("serve-static");
-const path = require("path");
-
+const express = require('express');
+const path = require('path');
 const app = express();
 
-//here we are configuring dist to serve app files
-app.use("/", serveStatic(path.join(__dirname, "/dist")));
+app.use(express.static(path.join(__dirname, 'build')));
 
-// this * route is to serve project on different page routes except root `/`
-app.get(/.*/, function(req, res) {
-  res.sendFile(path.join(__dirname, "/build/index.html"));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-const port = process.env.PORT || 10000;
-app.listen(port);
-console.log(`app is listening on port: ${port}`);
+app.listen(9000);
